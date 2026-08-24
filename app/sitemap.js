a47d7b41ccb5b5da5025857b4cceb8d22f1c0a16
juned pathan <@junedp068@gmail.com>
2026-08-24 15:50:06 +0530

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.voltaratechnologies.site",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.voltaratechnologies.site/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.voltaratechnologies.site/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.voltaratechnologies.site/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.voltaratechnologies.site/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
